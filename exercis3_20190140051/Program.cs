826cf0afa92dcea53cc0c4240005951d4fea6b4a
Rifa <rifafahim21.com@gmial.com>
2022-11-24 16:52:30 +0700

    commit

--- exercis3_20190140051/Program.cs
+++ exercis3_20190140051/Program.cs
@@ -167,7 +167,14 @@ namespace exercis3_20190140051
             {
                 try
                 {
-                    Console.WriteLine("\nMenu")
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. View all the records in the list");
+                    Console.WriteLine("3. Delete a record from the list");
+                    Console.WriteLine("4. Display the first record in the list");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit");
+                    Console.Write("\nEnter your choice (1-6): ");
                 }
             }
         }
